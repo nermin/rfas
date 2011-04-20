29758432afef86a0a890655f101263a9f0398b7c
Nermin Serifovic <nermin.s@gmail.com>
2011-04-20 08:53:03 -0400

    ventsink implementation - adding java library path

--- ventsink/project/build/ZeroMQ.scala
+++ ventsink/project/build/ZeroMQ.scala
@@ -2,6 +2,9 @@ import java.io.File
 import sbt._
 class ZeroMQ(info: ProjectInfo) extends DefaultProject(info) {
   System.setProperty("push.bind", "5557")
+  // On Linux, this also needs to be specified in LD_LIBRARY_PATH:
+  System.setProperty("java.library.path", "/usr/local/lib")
+
 
   val zmqLib = new File("/usr/local/share/java/zmq.jar")
   val zmqLibPath = Path.fromFile(zmqLib)
