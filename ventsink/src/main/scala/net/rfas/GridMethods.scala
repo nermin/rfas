387f8317d8317db2bde657043d3906748bdeedbf
Nermin Serifovic <nermin.s@gmail.com>
2011-04-24 14:09:27 -0400

    documentation

--- ventsink/src/main/scala/net/rfas/GridMethods.scala
+++ ventsink/src/main/scala/net/rfas/GridMethods.scala
@@ -10,7 +10,6 @@ object GridMethods {
     def grid = new GridList(l)
   }
 
-  //TODO proper ordering of results withing collection
   //TODO Error Handling
   //TODO Handle node failures
   //TODO Properly comment out the code
@@ -18,10 +17,5 @@ object GridMethods {
   //TODO Refactor the code to eliminate redundant lines
   //TODO Use Executor Service to multi-thread the worker
   //TODO Implement more methods beyond map
-  //TODO Replace hashmap/queue/sender/receiver with actors
-  //TODO Remote Class Loading
-  //TODO Documentation:
-  //          0. ZeroMQ must be installed on both sides (and LD_LIBRARY_PATH defined)
-  //          1. The client app must have ventsink jar on the classpath
-  //          2. Worker must have client jar on the classpath (due to lack of remote class-loading)
+
 }
