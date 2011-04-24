c2f6245f807927f900737529491490005cb46930
Nermin Serifovic <nermin.s@gmail.com>
2011-04-24 16:28:52 -0400

    filter implementation

--- ventsink/src/main/scala/net/rfas/collection/GridList.scala
+++ ventsink/src/main/scala/net/rfas/collection/GridList.scala
@@ -6,12 +6,14 @@ import java.util.UUID
 import net.rfas.GridCoreOps
 
 class GridList[+A](seqList: List[A]) {
+
+  GridCoreOps.noop
+
   def map[B](f: (A) => B): List[B] = {
     val signature = getSignature(f)
     // to uniquely identify this grid collection
     val sUUID = UUID.randomUUID
 
-    GridCoreOps.noop
     for (i <- 0 until seqList.size) {
         val baos = new ByteArrayOutputStream
         val oos = new ObjectOutputStream(baos)
@@ -38,6 +40,38 @@ class GridList[+A](seqList: List[A]) {
     GridCoreOps.done(sUUID)
 
     results.result.sortBy(_._1).map(_._2)
+  }
+
+  def filter(p: (A) => Boolean): List[A] = {
+    val signature = getSignature(p)
+    val sUUID = UUID.randomUUID
+
+    for (i <- 0 until seqList.size) {
+      val baos = new ByteArrayOutputStream
+      val oos = new ObjectOutputStream(baos)
+
+      try {
+        oos.writeObject(sUUID)
+        oos.writeInt(i)
+        oos.writeObject(seqList(i))
+        oos.writeObject(signature)
+        oos.writeObject(p)
+      } catch {
+          case e: Exception => e.printStackTrace //TODO handle this better
+      } finally {
+        oos.close
+      }
+
+      GridCoreOps.send(sUUID, baos.toByteArray)
+    }
+
+    val results = new ListBuffer[(Int, Boolean)]
+    while (results.size < seqList.size) {
+      results += GridCoreOps.receive(sUUID).asInstanceOf[(Int, Boolean)]
+    }
+    GridCoreOps.done(sUUID)
+
+    for (result <- results.result.sortBy(_._1) if result._2) yield seqList(result._1)
   }
 
   private def getSignature[T](f: T)(implicit m: ClassManifest[T]) = m.toString
